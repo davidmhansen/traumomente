ea3b91aa8a5f73dfaef33af58abb6152d9b4249d
David Hansen <david@davidhansen.de>
2025-04-04 11:42:47 +0200

    added current year logic in banner component

--- components/Banner/Banner.js
+++ components/Banner/Banner.js
@@ -1,12 +1,16 @@
 import Link from "next/link";
 
 export default function Banner() {
+  // Get current year and convert to string
+  const currentYear = new Date().getFullYear().toString();
+
   return (
     <div className="flex flex-col items-center justify-center w-full gap-4 px-8 py-3 text-center bg-apricot lg:px-16 lg:py-3 sm:text-left sm:flex-row">
       <p className="text-base text-white-linen">
-        Gute Nachrichten! Für <span className="font-semibold">2024</span> sind
-        noch <span className="font-semibold">freie Plätze</span> verfügbar. Ich
-        freue mich darauf, eure Geschichten zu hören!
+        Gute Nachrichten! Für{" "}
+        <span className="font-semibold">{currentYear}</span> sind noch{" "}
+        <span className="font-semibold">freie Plätze</span> verfügbar. Ich freue
+        mich darauf, eure Geschichten zu hören!
       </p>
       <Link
         href="mailto:ilona@trau-momente.de"
